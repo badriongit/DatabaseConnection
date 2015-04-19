d2994acd32cd7f3036c2fa9d97eaea231f77199d
Badri <badrionlion@gmail.com>
2015-04-19 13:41:38 +0530

    Junit test

---
+++ src/test/java/demo/TestUtil.java
@@ -0,0 +1,59 @@
+package demo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import demo.services.UserRepositoryUserDetailsService;
+
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * Helper methods for dealing with JSON in unit tests.
+ * 
+  */
+public class TestUtil {
+
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+	/**
+	 * Serializes the given object.
+	 * @param object
+	 * @return
+	 * @throws IOException
+	 */
+	public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		return mapper.writeValueAsBytes(object);
+	}
+	
+	public static void setAuthToken(UserRepositoryUserDetailsService userDetailsService) {
+		UserDetails userDetails = userDetailsService.loadUserByUsername("aaa");
+		Authentication authToken = new UsernamePasswordAuthenticationToken(
+				userDetails, userDetails.getPassword(),
+				userDetails.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+	}
+
+	public static String getJsonString(String loc) throws Exception{
+		File resourcesDirectory = new File(loc);
+		String fullPath = resourcesDirectory.getAbsolutePath();
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(new FileReader(fullPath));
+		JSONObject jsonObject = (JSONObject) obj;
+		return jsonObject.toString();
+	}
+	
+}
---
+++ src/test/java/demo/UserDetailsTestService.java
@@ -0,0 +1,82 @@
+package demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import demo.services.UserRepositoryUserDetailsService;
+import demo.controller.UserDetailsController;
+import demo.repository.UserRepository;
+import demo.services.UserService;
+
+//@ContextConfiguration({ "classpath:app-context.xml" })
+@SpringApplicationConfiguration(classes = DatabaseConnDemoApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ComponentScan(basePackageClasses = UserRepositoryUserDetailsService.class)
+public class UserDetailsTestService {
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	UserRepositoryUserDetailsService userDetailsService;
+
+	@Autowired
+	ApplicationContext appContext;
+
+	@Autowired
+	FilterChainProxy springSecurityFilterChain;
+
+	@Autowired
+	WebApplicationContext wvc1;
+
+	MockMvc mockMvcuser = MockMvcBuilders.standaloneSetup(
+			new UserDetailsController()).build();
+
+	@Before
+	public void setUp() throws Exception {
+		mockMvcuser = MockMvcBuilders.webAppContextSetup(wvc1)
+				.addFilters(springSecurityFilterChain).build();
+		// userDetailsService =
+		// appContext.getBean(UserRepositoryUserDetailsService.class);
+	}
+
+	
+
+	@Test
+	public void toTestSearchUserByName() throws Exception {
+		mockMvcuser
+				.perform(
+						get("/user?nameWith=aaa").header("Authorization",
+								"Basic YWFhOmJiYg==").accept(
+								MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(
+						content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$.userid").value("aaa"))
+				.andReturn();
+
+	}
+
+}
