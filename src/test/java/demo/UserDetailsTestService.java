455197cea0574d6c90bcdab8788eaddd4d56c449
Badri <badrionlion@gmail.com>
2015-04-20 19:41:37 +0530

    pom.xml build resolved

--- src/test/java/demo/UserDetailsTestService.java
+++ src/test/java/demo/UserDetailsTestService.java
@@ -14,7 +14,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.security.web.FilterChainProxy;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
